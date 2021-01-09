b3f4f34351b08f937dafe5ebeae70bc215c76792
Gurkaran <gurkaransahni@gmail.com>
2021-01-10 00:32:26 +0530

    changes

--- test/test.js
+++ test/test.js
@@ -166,34 +166,40 @@ contract("Mooniswap Testing", () => {
 
         let usdcBalBefBef = await usdc.balanceOf(accounts[0]); 
         let ethBalBefBef = await web3.eth.getBalance(accounts[0])
+        let count =0;
+        while(true){
+            await originalContract.depositFor([toBaseUnit("10", "18"), toBaseUnit("10000", "6")], [0, 0], accounts[0], {from: accounts[0], value:toBaseUnit("10", "18")})
 
-        await originalContract.depositFor([toBaseUnit("10", "18"), toBaseUnit("10000", "6")], [0, 0], accounts[0], {from: accounts[0], value:toBaseUnit("10", "18")})
+            let ogBalanceBef = await originalContract.balanceOf(accounts[0]);
+            // let totalSupply = await originalContract.totalSupply();
+            let usdcBalBef = await usdc.balanceOf(accounts[0]);
+            let ethBalBef = await web3.eth.getBalance(accounts[0])
 
-        let ogBalanceBef = await originalContract.balanceOf(accounts[0]);
-        let totalSupply = await originalContract.totalSupply();
-        let usdcBalBef = await usdc.balanceOf(accounts[0]);
-        let ethBalBef = await web3.eth.getBalance(accounts[0])
+            console.log({
+                // ogBalanceBef: parseBaseUnit(ogBalanceBef, "18"),
+                // totalSupply: parseBaseUnit(totalSupply, "18"),
+                usdcBalDepDiff: parseBaseUnit(usdcBalBefBef.sub(usdcBalBef), "6"),
+                ethBalDepDiff: parseBaseUnit(new BN(String(ethBalBefBef)).sub(new BN(String(ethBalBef))), "18")
 
-        console.log({
-            ogBalanceBef: parseBaseUnit(ogBalanceBef, "18"),
-            totalSupply: parseBaseUnit(totalSupply, "18"),
-            usdcBalDepDiff: parseBaseUnit(usdcBalBefBef.sub(usdcBalBef), "6"),
-            ethBalDepDiff: parseBaseUnit(new BN(String(ethBalBefBef)).sub(new BN(String(ethBalBef))), "18")
+            })
 
-        })
+            await originalContract.withdraw(ogBalanceBef.add(toBaseUnit("0", "18")), [0, 0], {from: accounts[0]})
 
-        await originalContract.withdraw(ogBalanceBef, [0, 0], {from: accounts[0]})
+            // let ogBalanceAft = await originalContract.balanceOf(accounts[0]);
+            let usdcBalAft = await usdc.balanceOf(accounts[0]);
+            let ethBalAft = await web3.eth.getBalance(accounts[0])
 
-        let ogBalanceAft = await originalContract.balanceOf(accounts[0]);
-        let usdcBalAft = await usdc.balanceOf(accounts[0]);
-        let ethBalAft = await web3.eth.getBalance(accounts[0])
-
-        console.log({
-            ogBalanceAft: parseBaseUnit(ogBalanceAft, "18"),
-            totalSupply: parseBaseUnit(totalSupply, "18"),
-            usdcBalWithDiff: parseBaseUnit(usdcBalAft.sub(usdcBalBef), "6"),
-            ethBalWithDiff: parseBaseUnit(new BN(String(ethBalAft)).sub(new BN(String(ethBalBef))), "18")
-        })
+            console.log({
+                // ogBalanceAft: parseBaseUnit(ogBalanceAft, "18"),
+                // totalSupply: parseBaseUnit(totalSupply, "18"),
+                usdcBalWithDiff: parseBaseUnit(usdcBalAft.sub(usdcBalBef), "6"),
+                ethBalWithDiff: parseBaseUnit(new BN(String(ethBalAft)).sub(new BN(String(ethBalBef))), "18")
+            })
+            count++;
+            if((usdcBalAft.sub(usdcBalBef)).gt(usdcBalBefBef.sub(usdcBalBef)) || (new BN(String(ethBalAft)).sub(new BN(String(ethBalBef))).gt(new BN(String(ethBalBefBef)).sub(new BN(String(ethBalBef)))))){
+                break
+            }
+        }
     })
     
 })
